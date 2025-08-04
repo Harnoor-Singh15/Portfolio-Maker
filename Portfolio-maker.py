24f56e6584b997f8429ad63ddfeeff6b1005ca6b
Harnoor Singh <harnoorharnoor23@gmail.com>
2025-08-04 21:22:56 +0530

    Create Portfolio-maker.py

---
+++ Portfolio-maker.py
@@ -0,0 +1,14 @@
+#Created by ~ Harnoor Singh 
+
+A = input("Enter your full name :  ")
+B = input("Enter your age :  ")
+C = input("Enter your gender :  ")
+D = input("Enter your interests :  ")
+E = input("Enter your life aim :  ")
+
+print("\n")
+print("Myself ",A)
+print("My age is ",B)
+print("My gender is ",C)
+print("My interests are ",D)
+print("I want to become ",E)
